86474daba214196197a2c226a5bcadd7e970272c
Zackeus Zetterberg <zackeua@gmail.com>
2022-01-30 12:44:52 +0100

    Fixade dag 12 2015

---
+++ 2015/dag12/12_2.py
@@ -0,0 +1,34 @@
+import sys
+import json
+
+def sum_list(l: list) -> int:
+    result = 0
+    for i in l:
+        if type(i) == int:
+            result += i
+        elif type(i) == list:
+            result += sum_list(i)
+        elif type(i) == dict:
+            result += sum_dict(i)
+    return result
+
+def sum_dict(d: dict) -> int:
+    result = 0
+    red = True
+    for i in d:
+        if type(d[i]) == int:
+            result += d[i]
+        elif d[i] == 'red':
+            red = False
+        elif type(d[i]) == list:
+            result += sum_list(d[i])
+        elif type(d[i]) == dict:
+            result += sum_dict(d[i])
+    return result * red
+
+
+with open(sys.argv[1], 'r') as f:
+    data = json.load(f)
+
+print(sum_list(data))
+    
