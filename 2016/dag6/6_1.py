99ac6aa66cb6f8805c2b719352f651e03f86a1c2
Zackeus Zetterberg <zackeua@gmail.com>
2022-09-07 23:21:25 +0200

    Fixade dag 4-6 2016

---
+++ 2016/dag6/6_1.py
@@ -0,0 +1,20 @@
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    data = f.readlines()
+    data = [row.strip() for row in data]
+    print(data)
+
+words = [[] for _ in range(len(data[0]))]
+
+for i, _ in enumerate(data[0]):
+    for j, _ in enumerate(data):
+        words[i].append(data[j][i])
+
+counts = [[(letter, row.count(letter)) for letter in 'abcdefghijklmnopqrtsuvwxyz'] for row in words]
+
+counts = [sorted(row, key=lambda x:x[1], reverse=True) for row in counts]
+
+result = ''.join([counts[i][0][0] for i in range(len(data[0]))])
+
+print(result)
