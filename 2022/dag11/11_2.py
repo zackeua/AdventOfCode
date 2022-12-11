5e6fe9795405bd47b44c329c9270b73f3d8d09c2
Zackeus Zetterberg <zackeua@gmail.com>
2022-12-11 08:31:44 +0100

    Fixade dag 11 2022

---
+++ 2022/dag11/11_2.py
@@ -0,0 +1,89 @@
+import sys
+
+from functools import partial
+from functools import reduce
+
+debug = False
+
+
+def debug_print(*s, **args):
+    if debug:
+        print(*s, **args)
+
+
+def update_func(old, monkey, update_rule, send_rule, true_list, false_list):
+    new = update_rule[monkey](old)
+
+    new = int(new %(reduce(mul, send_rule, 1)))
+
+    if new % send_rule[monkey] == 0:
+        return new, true_list[monkey]
+    else:
+        return new, false_list[monkey]
+
+
+def add(b, a): return a + b
+def mul(b, a): return a * b
+
+
+def power(a, ): return a * a
+
+
+def main():
+
+    with open(sys.argv[1], 'r') as f:
+        data = f.readlines()
+        data = [line.strip() for line in data]
+        monkey_items = [list(map(int, row.split(': ')[1].split(', ')))
+                        for row in data[1::7]]
+        update_rule = []
+        for line in data[2::7]:
+            print(line)
+            sub_line = line.split(' = ')[1]
+            if sub_line == 'old * old':
+                update_rule.append(power)
+            elif '*' in sub_line:
+                update_rule.append(partial(mul, int(sub_line.split('*')[-1])))
+                print(update_rule[-1](1))
+            else:
+                update_rule.append(partial(add, int(sub_line.split('+')[-1])))
+                print(update_rule[-1](1))
+
+        send_rule = [int(row.split(' ')[-1]) for row in data[3::7]]
+        monkey_true_list = [int(row[-1]) for row in data[4::7]]
+        monkey_false_list = [int(row[-1]) for row in data[5::7]]
+
+        print(monkey_items)
+        print(update_rule)
+        print(send_rule)
+        print(monkey_true_list)
+        print(monkey_false_list)
+
+    monkey_inspect_count = [0]*len(monkey_items)
+    for round in range(1, 10001):
+        if round%1000 == 0:
+            pass
+        print(round)
+        for monkey in range(len(monkey_items)):
+            new_monkey_items = []
+            debug_print(f'Monkey {monkey}:')
+            for worry in monkey_items[monkey]:
+                debug_print(f'  Monkey inspects an item with a worry level of {worry}.')
+                next_worry, next_monkey = update_func(worry, monkey, update_rule, send_rule, monkey_true_list, monkey_false_list)
+                monkey_inspect_count[monkey] += 1
+                if monkey == next_monkey:
+                    new_monkey_items.append(next_worry)
+                else:
+                    monkey_items[next_monkey].append(next_worry)
+            monkey_items[monkey] = new_monkey_items
+        debug_print(f'After round {round}, the monkeys are holding items with these worry levels:')
+        for monkey in range(len(monkey_items)):
+            debug_print(f'Monkey {monkey}: {", ".join(list(map(str,monkey_items[monkey])))}')
+        #input()
+    print(monkey_inspect_count)
+    monkey_inspect_count.sort()
+    print(monkey_inspect_count[-1] * monkey_inspect_count[-2])
+
+
+if __name__ == '__main__':
+    main()
