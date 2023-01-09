b6c3bdaa78bad01abbabf5f48cba135b76880d82
Zackeus Zetterberg <zackeua@gmail.com>
2023-01-09 18:10:58 +0100

    Fixade script som genererar en stl fil från dag 12 input.

--- 2022/generate_stl.py
+++ 2022/generate_stl.py
@@ -11,34 +11,36 @@ def get_z(c):
         c = 'z'
     return ord(c) - 96
 
-def get_vertices(x, y, z):
+
+def get_vertices(x, y, z, base_height = 10):
     return np.array([[x, y, 0],
                      [x+1, y, 0],
                      [x+1, y+1, 0],
                      [x, y+1, 0],
-                     [x, y, z],
-                     [x+1, y, z],
-                     [x+1, y+1, z],
-                     [x, y+1, z]])
+                     [x, y, z + base_height],
+                     [x+1, y, z + base_height],
+                     [x+1, y+1, z + base_height],
+                     [x, y+1, z + base_height]])
+
 
 def get_faces(x, y, z):
-    return np.array([[0,3,1],
-                     [1,3,2],
-                     [0,4,7],
-                     [0,7,3],
-                     [4,5,6],
-                     [4,6,7],
-                     [5,1,2],
-                     [5,2,6],
-                     [2,3,6],
-                     [3,7,6],
-                     [0,1,5],
-                     [0,5,4]])
+    return np.array([[0, 3, 1],
+                     [1, 3, 2],
+                     [0, 4, 7],
+                     [0, 7, 3],
+                     [4, 5, 6],
+                     [4, 6, 7],
+                     [5, 1, 2],
+                     [5, 2, 6],
+                     [2, 3, 6],
+                     [3, 7, 6],
+                     [0, 1, 5],
+                     [0, 5, 4]])
 
 
 def example():
     # Define the 8 vertices of the cube
-    vertices = np.array([\
+    vertices = np.array([
         [-1, -1, -1],
         [+1, -1, -1],
         [+1, +1, -1],
@@ -48,26 +50,34 @@ def example():
         [+1, +1, +1],
         [-1, +1, +1]])
 
+    vertices2 = np.array([[vertex[0]+3, vertex[1], vertex[2]] for vertex in vertices])
+
     # Define the 12 triangles composing the cube
-    faces = np.array([\
-        [0,3,1],
-        [1,3,2],
-        [0,4,7],
-        [0,7,3],
-        [4,5,6],
-        [4,6,7],
-        [5,1,2],
-        [5,2,6],
-        [2,3,6],
-        [3,7,6],
-        [0,1,5],
-        [0,5,4]])
+    faces = np.array([
+        [0, 3, 1],
+        [1, 3, 2],
+        [0, 4, 7],
+        [0, 7, 3],
+        [4, 5, 6],
+        [4, 6, 7],
+        [5, 1, 2],
+        [5, 2, 6],
+        [2, 3, 6],
+        [3, 7, 6],
+        [0, 1, 5],
+        [0, 5, 4]])
 
     # Create the mesh
-    cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+    cube = mesh.Mesh(np.zeros(faces.shape[0]*2, dtype=mesh.Mesh.dtype))
+    offset = 0
     for i, f in enumerate(faces):
         for j in range(3):
-            cube.vectors[i][j] = vertices[f[j],:]
+            cube.vectors[i][j] = vertices[f[j], :]
+        offset = i
+
+    for i, f in enumerate(faces):
+        for j in range(3):
+            cube.vectors[i + offset+1][j] = vertices2[f[j], :]
 
     # Write the mesh to file "cube.stl"
     cube.save('cube.stl')
@@ -75,7 +85,6 @@ def example():
 
 def main():
 
-    side_length = 1
     with open(sys.argv[1], 'r') as f:
         faces = get_faces(0, 0, 0)
         data = f.readlines()
@@ -86,16 +95,21 @@ def main():
         print(length)
         print(width)
         STL_MESH = mesh.Mesh(np.zeros(faces.shape[0] * length * width, dtype=mesh.Mesh.dtype))
+        offset = 0
+        next_offset = 0
         for x, row in enumerate(data):
             for y, elem in enumerate(row):
                 z = get_z(elem)
                 vertecies = get_vertices(x, y, z)
+                offset = next_offset
                 for i, f in enumerate(get_faces(x, y, z)):
                     for j in range(3):
-                        STL_MESH.vectors[x * width + y + i][j] = vertecies[f[j], :]
-        print(STL_MESH.vectors)
-        STL_MESH.save('cube.stl')
+                        STL_MESH.vectors[i + offset][j] = vertecies[f[j], :]
+                    next_offset += 1
+        STL_MESH.save(sys.argv[2])
 
 
 if __name__ == '__main__':
     main()
+    #example()
+    
