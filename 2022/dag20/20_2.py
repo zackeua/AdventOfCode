ea2c2c9424128ae056e549080d2c1b08c72a0687
Zackeus Zetterberg <zackeua@gmail.com>
2023-04-07 01:01:35 +0200

    Började med del 2 dag 20 2022

---
+++ 2022/dag20/20_2.py
@@ -0,0 +1,110 @@
+import sys
+
+debug = True
+
+
+decryption_key  = 811589153
+
+
+class Number:
+    def __init__(self, original_position, value) -> None:
+        self._original_position = original_position
+        self._value = value
+        self._current_position = original_position
+
+    
+    def copy(self):
+        result = Number(self._original_position, self._value)
+        result._current_position = self._current_position
+        return result
+
+
+def debug_print(*s, **args):
+    if debug:
+        print(*s, **args)
+
+def show_array(array):
+    debug_print(', '.join([str(elem._value) for elem in array]))
+
+
+
+def shuffle(data: list[Number]):
+
+    length = len(data)
+
+    order = [elem[0] for elem in sorted([(index, elem._original_position) for index, elem in enumerate(data)], key=lambda x: x[1])]
+
+    for elem in order: # the original order of the elements
+
+        for number in data: # get current position
+            if number._original_position == elem:
+                i = number._current_position
+        
+
+        current_element = data[i].copy()
+        assert(i == current_element._current_position)
+
+        new_position = i + current_element._value
+
+
+        if new_position < 0:
+            new_position = new_position + new_position // length
+        if new_position > length:
+            new_position = new_position + new_position // length
+
+        #new_position = new_position + new_position // length
+       
+        new_position = new_position % length
+
+        if new_position < i: # move everything from new_position to i up one step
+            for pos in range(i, new_position, -1):
+                data[pos] = data[pos - 1]
+                data[pos]._current_position = pos
+
+
+        elif i < new_position: # move everything from i to new_position down one step
+            for pos in range(i, new_position):
+                data[pos] = data[pos + 1]
+                data[pos]._current_position = pos
+
+
+
+        data[new_position] = current_element
+        data[new_position]._current_position = new_position
+
+    for index, elem in enumerate(data):
+        data[i]._current_position = index
+            
+
+    return data
+
+def main():
+
+    with open(sys.argv[1], 'r') as f:
+        data = [Number(index, decryption_key * int(elem)) for index, elem in enumerate(f.readlines())]
+        
+
+        debug_print('Initial arrangement:')
+        show_array(data)
+        debug_print()
+        for i in range(1, 10 + 1):
+            data = shuffle(data)
+            debug_print(f'After {i} round{"s" if i > 1 else ""} of mixing:')
+            show_array(data)
+            debug_print()
+
+
+        index_of_0 = [elem._value for elem in data].index(0)
+        for elem in data:
+            if elem._value == 0:
+                assert(index_of_0 == elem._current_position)
+
+        
+        print(data[(index_of_0 + 1000) % len(data)]._value)
+        print(data[(index_of_0 + 2000) % len(data)]._value)
+        print(data[(index_of_0 + 3000) % len(data)]._value)
+        print(sum([data[(index_of_0 + i) % len(data)]._value for i in [1000, 2000, 3000]]))
+
+
+if __name__ == '__main__':
+    main()
