ad8a7d0a9ecf489d314cb4b0ee3a08e8f2df46da
Zackeus Zetterberg <zackeua@gmail.com>
2023-08-29 22:38:14 +0200

    Fixade dag 19 del 2 2015

--- 2015/dag19/19_2.py
+++ 2015/dag19/19_2.py
@@ -1,34 +1,23 @@
 import sys
 
-import heapq
 
 
 def get_steps(molecule: str, goal: str, rules: dict):
 
-    visited = set()
-    while molecule != []:
-        #result, old_molecule = molecule[0]
-        #molecule = molecule[1:]
+    steps = 0
 
-        result, old_molecule = heapq.heappop(molecule)
-
-        visited.add(old_molecule)
+    prev = ""
+    while molecule != goal:
 
         for key in rules:
-            if rules[key] != 'e':                
-                current_molecule = old_molecule.replace(key, rules[key], 1)
-                if current_molecule not in visited:
-                    heapq.heappush(molecule, (1 + result, current_molecule))
-                    #molecule.append((1 + result, current_molecule))
-                #print(old_molecule, current_molecule, key, rules[key])
+            if key in molecule:
+                molecule = molecule.replace(key, rules[key], 1)
+                steps += 1
+        if molecule == prev:
+            return steps
+        prev = molecule
+    return steps
 
-            elif old_molecule == key:
-                old_molecule = 'e'
-                #print(old_molecule, current_molecule, key, rules[key])
-                return result + 1
-        #print(molecule)
-        #input()
-        #molecule.sort()
 
 def main():
 
@@ -41,14 +30,12 @@ def main():
         rules = {}
 
         for rule in data[:-2]:
-            val, key = rule.split(' => ')
-            rules[key[::-1]] = val[::-1]
-        queue = [(0, molecule[::-1])]
-        heapq.heapify(queue)
+            key, val = rule.split(' => ')
+            rules[val] = key
         goal = 'e'
-        print(rules)
-        print(molecule)
-        print(get_steps(queue, goal, rules))
+        #print(rules)
+        #print(molecule)
+        print(get_steps(molecule, goal, rules))
 
 if __name__ == '__main__':
     main()
