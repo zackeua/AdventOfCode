ca4ecb10b8e466ba905640807fd1cd1c1f030e29
Zackeus Zetterberg <zackeua@gmail.com>
2023-12-13 08:32:21 +0100

    Progress med del 2 dag 12 2023

--- 2023/dag12/12_1.py
+++ 2023/dag12/12_1.py
@@ -4,7 +4,6 @@ import re
 def replace_unknown(text: str):
     if '?' not in text:
         return [text]
-    
 
     tmp1 = text.replace('?', '#', 1)
     tmp2 = text.replace('?', '.', 1)
@@ -13,11 +12,11 @@ def replace_unknown(text: str):
 
     return result
 
-def create_regex(pattern:str):
+
+def create_regex(pattern: str):
     numbers = list(map(int, pattern.split(',')))
-    regex_pattern = '^\.*' +'\.+'.join(['#'*number for number in numbers]) + '\.*$'
+    regex_pattern = r'^\.*' + r'\.+'.join(['#'*number for number in numbers]) + r'\.*$'
     return regex_pattern
-    
 
 
 def main():
@@ -27,16 +26,20 @@ def main():
         data = [line.strip().split() for line in data]
         total = 0
         for springs, pattern in data:
+            local = 0
             regex_pattern = create_regex(pattern)
             regex_matcher = re.compile(regex_pattern)
-            #print(springs, pattern, regex_pattern)
+            # print(springs, pattern, regex_pattern)
             for spring in replace_unknown(springs):
                 result = regex_matcher.match(spring)
                 if result is not None:
-                    #print(spring, regex_pattern)  
-                    total += 1
-                
+                    # print(spring, regex_pattern)
+                    local += 1
+            print(f'{springs}, {pattern} : {local}')
+            total += local
         print(total)
+
 
 if __name__ == '__main__':
     main()
+
--- 2023/dag12/12_2.py
+++ 2023/dag12/12_2.py
@@ -1,22 +1,41 @@
 import sys
 import re
 import tqdm
+import functools
 
-def replace_unknown(text: str):
-    if '?' not in text:
-        return [text]
-    tmp1 = text.replace('?', '#', 1)
-    tmp2 = text.replace('?', '.', 1)
-    result = replace_unknown(tmp1)
-    result.extend(replace_unknown(tmp2))
 
-    return result
+DAMAGED = '#'
+OPERATIONAL = '.'
+UNKNOWN = '?'
 
-def create_regex(pattern:str):
-    numbers = list(map(int, pattern.split(',')))
-    extended_numbers = numbers*5
-    regex_pattern = '^\.*' +'\.+'.join(['#'*number for number in extended_numbers]) + '\.*$'
-    return regex_pattern
+
+# @functools.lru_cache()
+def matches(text: str, pattern: list[int], prev='.', debug_str=''):
+    if len(pattern) == 0:
+        if DAMAGED not in text:
+            assert debug_str != ''
+            print(debug_str)
+            return 1
+        return 0
+    if len(text) == 0:
+        return 0
+    part = text[:pattern[0]]
+    # print(text, pattern, part)
+    # input()
+    total = 0
+    # matches the # and ? in the pattern
+    if prev != DAMAGED and OPERATIONAL not in part and len(part) == pattern[0]:
+        # print(part, pattern, text)
+        # The # has to be separated by . or ?
+        if pattern[0] < len(text) and DAMAGED != text[pattern[0]]:
+            total += matches(text[pattern[0]+1:], pattern[1:], text[pattern[0]], debug_str + '#'*pattern[0] + '.')
+        # if match is at the end of the text, there is nothing to the right
+        elif pattern[0] == len(text) and len(pattern) == 1:
+            total += matches(text[pattern[0]+1:], pattern[1:], '.', debug_str + '#'*pattern[0])
+    # assume the pattern did not match, try again
+    total += matches(text[1:], pattern, text[0], debug_str + '.')
+    return total
+
 
 def main():
 
@@ -24,18 +43,16 @@ def main():
         data = f.readlines()
         data = [line.strip().split() for line in data]
         total = 0
-        for springs, pattern in data:
-            regex_pattern = create_regex(pattern)
-            regex_matcher = re.compile(regex_pattern)
-            extended_springs = '?'.join([springs]*5)
-            print(extended_springs, pattern, regex_pattern)
-            for spring in tqdm.tqdm(replace_unknown(extended_springs)):
-                result = regex_matcher.match(spring)
-                if result is not None:
-                    #print(spring, regex_pattern)  
-                    total += 1
-                
+        for springs, pattern in tqdm.tqdm(data):
+            local = 0
+            pattern = tuple(map(int, pattern.split(',')))
+            extended_springs = '?'.join([springs])
+            local = matches(extended_springs, pattern)
+            total += local
+            print(f'{springs}, {pattern} : {local}')
         print(total)
+        assert total < 203284796184157
+        assert total < 1907721526566707227243
 
 if __name__ == '__main__':
     main()
