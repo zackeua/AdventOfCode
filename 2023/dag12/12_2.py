09033a845188b73068fd6776e7149b1114c72424
Zackeus Zetterberg <zackeua@gmail.com>
2023-12-14 21:37:58 +0100

    Fixade del 2 dag 12 2023

--- 2023/dag12/12_2.py
+++ 2023/dag12/12_2.py
@@ -8,13 +8,19 @@ DAMAGED = '#'
 OPERATIONAL = '.'
 UNKNOWN = '?'
 
+# global matcher
 
 @functools.lru_cache()
-def matches(text: str, pattern: list[int], prev='.', debug_str=''):
+def matches(text: str, pattern: list[int], prev='.', debug_str=()):
     if len(pattern) == 0:
         if DAMAGED not in text:
-            assert debug_str != ''
-            # print(debug_str)
+            # global matcher
+            #if matcher.match(''.join(debug_str) + text) is None:
+            #    print(''.join(debug_str) + text)
+            #    sys.exit(0)
+            #    # print(debug_str + text)
+            # assert debug_str != ''
+            # print(debug_str + text)
             return 1
         return 0
     if len(text) == 0:
@@ -28,18 +34,28 @@ def matches(text: str, pattern: list[int], prev='.', debug_str=''):
         # print(part, pattern, text)
         # The # has to be separated by . or ?
         if pattern[0] < len(text) and DAMAGED != text[pattern[0]]:
-            total += matches(text[pattern[0]+1:], pattern[1:], text[pattern[0]], debug_str + '#'*pattern[0] + '.')
+            total += matches(text[pattern[0]+1:], pattern[1:], text[pattern[0]]) #, debug_str + ('#'*pattern[0] + '.',))
         # if match is at the end of the text, there is nothing to the right
         elif pattern[0] == len(text) and len(pattern) == 1:
-            total += matches(text[pattern[0]+1:], pattern[1:], '.', debug_str + '#'*pattern[0])
+            total += matches(text[pattern[0]+1:], pattern[1:], '.') #, debug_str + ('#'*pattern[0],))
     # assume the pattern did not match, try again
-    total += matches(text[1:], pattern, text[0], debug_str + '.')
+    if prev != DAMAGED:
+        total += matches(text[1:], pattern, text[0]) #, debug_str + (text[0],))
     return total
 
+
+def generate_matcher(pattern: list[int], length: int):
+    regex = ''
+    m = r'[.?]+'.join(r'[?#]'*p for p in pattern)
+
+    regex = r'^[.?]*' + r'[.?]+'.join([m]*length) + r'[.?]*$'
+
+    return re.compile(regex)
 
 def main():
 
     with open(sys.argv[1], 'r') as f:
+        # global matcher
         data = f.readlines()
         data = [line.strip().split() for line in data]
         total = 0
@@ -48,9 +64,10 @@ def main():
             local = 0
             pattern = tuple(map(int, pattern.split(',')))
             extended_springs = '?'.join([springs]*repetitions)
+            # matcher = generate_matcher(pattern, repetitions)
             local = matches(extended_springs, pattern*repetitions)
             total += local
-            print(f'{springs}, {pattern} : {local}')
+            # print(f'{springs}, {pattern} : {extended_springs}, {pattern * repetitions} : {local}')
         print(total)
         assert total < 203284796184157
         assert total < 1907721526566707227243
