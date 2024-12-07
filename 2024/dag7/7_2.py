a02a0af3a15fc9ae2dab02d961782de16c4f318e
Zackeus Zetterberg <zackeua@gmail.com>
2024-12-07 06:28:32 +0100

    Fixade dag 7 2024

---
+++ 2024/dag7/7_2.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def search(target: int, current: int, equation: list[int]):
+
+    if not equation:
+        return target == current
+
+    equation_copy = [val for val in equation]
+    val = equation_copy[0]
+    equation_copy = equation_copy[1:]
+
+    if search(target, current+val, equation_copy):
+        return target
+    if search(target, current*val, equation_copy):
+        return target
+    c = int(str(current) + str(val))
+    if search(target, c, equation_copy):
+        return target
+
+    return 0
+
+
+def main():
+    with open(sys.argv[1], 'r') as f:
+        data = f.readlines()
+        data = [line.strip() for line in data]
+        data = [line.split(': ') for line in data]
+        data = [[int(line[0]), list(map(int, line[1].split()))]
+                for line in data]
+        print(data)
+        total = 0
+        for eq in data:
+            total += search(eq[0], 0, eq[1])
+        print(total)
+
+
+if __name__ == '__main__':
+    main()
