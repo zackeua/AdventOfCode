39dfab9545799613940cbb04ce62832353908982
Zackeus Zetterberg <zackeua@gmail.com>
2024-12-17 21:55:18 +0100

    Fixade dag 17 2024 del 2

--- 2024/dag17/17_2.py
+++ 2024/dag17/17_2.py
@@ -15,11 +15,16 @@ def get_val(registers, arg):
 def computer(program, registers, program_counter):
 
     output = []
+    # prev_program_counter = -1
     try:
         while True:
+            # if program_counter == prev_program_counter:
+            # return ','.join(output)
+            # prev_program_counter = program_counter
             if program_counter < 0 or program_counter > len(program):
-                print(','.join(output))
-                break
+                # print(','.join(output))
+                output
+                # break
             # print(program_counter)
             if program[program_counter] == 0:
                 # adv operator:
@@ -45,8 +50,7 @@ def computer(program, registers, program_counter):
                 program_counter += 2
             elif program[program_counter] == 5:
                 val = get_val(registers, program[program_counter + 1])
-                output.append(str(val % 8))
-                # print(','.join(output))
+                output.append(val % 8)
                 program_counter += 2
             elif program[program_counter] == 6:
                 numerator = registers[4]  # A
@@ -63,7 +67,9 @@ def computer(program, registers, program_counter):
             else:
                 assert False
     except:
-        print(','.join(output))
+        # print(','.join(output))
+        return output
+    return output
 
 
 def main():
@@ -87,7 +93,49 @@ def main():
         else:
             program = list(map(int, line.split()[1].split(',')))
 
-    computer(program, registers, program_counter)
+    a_val = 0
+    a_val += 5 * 8 ** 15
+    a_val += 3 * 8 ** 14
+    a_val += 2 * 8 ** 13
+    a_val += 2 * 8 ** 12
+    a_val += 3 * 8 ** 11
+    a_val += 5 * 8 ** 10
+    a_val += 0 * 8 ** 9
+    a_val += 1 * 8 ** 8
+    a_val += 3 * 8 ** 7
+    a_val += 4 * 8 ** 6
+    a_val += 0 * 8 ** 5
+    a_val += 3 * 8 ** 4
+    a_val += 6 * 8 ** 3
+    a_val += 0 * 8 ** 2
+    a_val += 1 * 8 ** 1
+    a_val += 7 * 8 ** 0
+    registers[4] = a_val
+    registers[5] = 0
+    registers[6] = 0
+    program_counter = 0
+    tmp = computer(program, registers, program_counter)
+    # print(program)
+    # print(tmp)
+
+    print(a_val)
+    assert len(tmp) == len(program)
+    assert program[15] == tmp[15], f'{program[15]} {tmp[15]}'
+    assert program[14] == tmp[14], f'{program[14]} {tmp[14]}'
+    assert program[13] == tmp[13], f'{program[13]} {tmp[13]}'
+    assert program[12] == tmp[12], f'{program[12]} {tmp[12]}'
+    assert program[11] == tmp[11], f'{program[11]} {tmp[11]}'
+    assert program[10] == tmp[10], f'{program[10]} {tmp[10]}'
+    assert program[9] == tmp[9], f'{program[9]} {tmp[9]}'
+    assert program[8] == tmp[8], f'{program[8]} {tmp[8]}'
+    assert program[7] == tmp[7], f'{program[7]} {tmp[7]}'
+    assert program[6] == tmp[6], f'{program[6]} {tmp[6]}'
+    assert program[5] == tmp[5], f'{program[5]} {tmp[5]}'
+    assert program[4] == tmp[4], f'{program[4]} {tmp[4]}'
+    assert program[3] == tmp[3], f'{program[3]} {tmp[3]}'
+    assert program[2] == tmp[2], f'{program[2]} {tmp[2]}'
+    assert program[1] == tmp[1], f'{program[1]} {tmp[1]}'
+    assert program[0] == tmp[0], f'{program[0]} {tmp[0]}'
 
 
 if __name__ == '__main__':
