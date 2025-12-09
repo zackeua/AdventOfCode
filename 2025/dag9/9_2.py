9659e24dd7df73cc6a2d9246a8a7ff29028ff10a
Zackeus Zetterberg <zackeua@gmail.com>
2025-12-10 00:06:51 +0100

    Försökt till linke intersect lösning

--- 2025/dag9/9_2.py
+++ 2025/dag9/9_2.py
@@ -37,90 +37,106 @@ def main():
     for elem in data:
         squares[elem] = "#"
 
-    for a, b in zip(data, data[1:] + [data[0]]):
-        mi_x = min(a[0], b[0])
-        ma_x = max(a[0], b[0])
-        mi_y = min(a[1], b[1])
-        ma_y = max(a[1], b[1])
-        x_diff = mi_x - ma_x
-        y_diff = mi_y - ma_y
-        if x_diff != 0:
-            for x in range(mi_x + 1, ma_x):
-                squares[(x, a[1])] = "X"
-
-        if y_diff != 0:
-            for y in range(mi_y + 1, ma_y):
-                squares[(a[0], y)] = "X"
-
-    to_color = [((min_x + max_x) // 2, (min_y + max_y) // 4)]
-    # to_color = [(data[0][0] + 1, data[0][1] + 1)]
-
-    i = 0
-    while tqdm.tqdm(to_color):
-        i += 1
-        if i % 10000 == 0:
-            plt.plot(
-                [a for a, b in squares.keys() if squares[(a, b)] in "#X"],
-                [b for a, b in squares.keys() if squares[(a, b)] in "#X"],
-                ".",
-            )
-            plt.show()
-
-        # print(to_color)
-        a, b = to_color.pop()
-        if (a + 1, b) not in squares and (a + 1, b) not in to_color:
-            to_color.append((a + 1, b))
-
-        if (a - 1, b) not in squares and (a - 1, b) not in to_color:
-            to_color.append((a - 1, b))
-
-        if (a, b + 1) not in squares and (a, b + 1) not in to_color:
-            to_color.append((a, b + 1))
-
-        if (a, b - 1) not in squares and (a, b - 1) not in to_color:
-            to_color.append((a, b - 1))
-        # print(to_color)
-        # input()
-        squares[(a, b)] = "X"
+    # for a, b in zip(data, data[1:] + [data[0]]):
+    #     mi_x = min(a[0], b[0])
+    #     ma_x = max(a[0], b[0])
+    #     mi_y = min(a[1], b[1])
+    #     ma_y = max(a[1], b[1])
+    #     x_diff = mi_x - ma_x
+    #     y_diff = mi_y - ma_y
+    #     if x_diff != 0:
+    #         for x in range(mi_x + 1, ma_x):
+    #             squares[(x, a[1])] = "X"
+    #
+    #     if y_diff != 0:
+    #         for y in range(mi_y + 1, ma_y):
+    #             squares[(a[0], y)] = "X"
 
     largest_area = 0
     for a, b in itertools.combinations(data, 2):
-        # print(a, b, area)
-        allowed_rectangle = True
-        # assert squares[a] == "#"
-        # assert squares[b] == "#"
-        if squares[(a[0], b[1])] == "." or squares[(b[0], a[1])] == ".":
-            continue
+        valid_rectangle = True
+        for elem1, elem2 in zip(data, data[1:] + [data[0]]):
+            # (a[0], a[1])              (a[0], b[1])
+            #
+            #                 X (elem1)
+            #                 |
+            # (b[0], a[1])    |         (b[0], b[1])
+            #                 |
+            #                 |
+            #                 |
+            #                 X (elem2)
 
-        for y in range(a[1], b[1]):
-            if squares[(a[0], y)] == "." or squares[(b[0], y)] == ".":
-                allowed_rectangle = False
+            line_min_x = min(elem1[0], elem2[0])
+            line_max_x = max(elem1[0], elem2[0])
+            line_min_y = min(elem1[1], elem2[1])
+            line_max_y = max(elem1[1], elem2[1])
+
+            # line 1: (a[0], a[1]) -> (a[0], b[1])
+            y_min = min(a[1], b[1])
+            y_max = max(a[1], b[1])
+            x = a[0]
+
+            if (
+                line_min_x < x < line_max_x
+                and y_min < line_min_y < y_max
+                and y_min < line_max_y < y_max
+            ):
+                valid_rectangle = False
                 break
-        if not allowed_rectangle:
-            continue
+            # line 2: (a[0], a[1]) -> (b[0], a[1])
+            x_min = min(a[0], b[0])
+            x_max = max(a[0], b[0])
+            y = a[1]
 
-        for y in range(a[0], b[0]):
-            if squares[(x, a[1])] == "." or squares[(x, b[1])] == ".":
-                allowed_rectangle = False
+            if (
+                line_min_y < y < line_max_y
+                and x_min < line_min_x < x_max
+                and x_min < line_max_x < x_max
+            ):
+                valid_rectangle = False
                 break
-        if not allowed_rectangle:
-            continue
+            # line 3: (b[0], a[1]) -> (b[0], b[1])
+            y_min = min(a[1], b[1])
+            y_max = max(a[1], b[1])
+            x = b[0]
+            if (
+                line_min_x < x < line_max_x
+                and y_min < line_min_y < y_max
+                and y_min < line_max_y < y_max
+            ):
+                valid_rectangle = False
+                break
 
+            # line 4: (a[0], b[1]) -> (b[0], b[1])
+            x_min = min(a[0], b[0])
+            x_max = max(a[0], b[0])
+            y = b[1]
+            if (
+                line_min_y < y < line_max_y
+                and x_min < line_min_x < x_max
+                and x_min < line_max_x < x_max
+            ):
+                valid_rectangle = False
+                break
+
+        if not valid_rectangle:
+            continue
         area = (abs(a[0] - b[0]) + 1) * (abs(a[1] - b[1]) + 1)
         if area > largest_area:
+            print(a, b, area)
             largest_area = area
 
     # plt.plot(
-    # [a for a, b in squares.keys() if squares[(a, b)] in "#X"],
-    # [b for a, b in squares.keys() if squares[(a, b)] in "#X"],
-    # ".",
+    #     [a for a, b in squares.keys() if squares[(a, b)] in "#X"],
+    #     [b for a, b in squares.keys() if squares[(a, b)] in "#X"],
+    #     ".",
     # )
     # plt.show()
-
+    #
     # for y in range(min_y, max_y):
-    # for x in range(min_x, max_x + 1):
-    # print(squares[(x, y)], end="")
-    # print()
+    #     for x in range(min_x, max_x + 1):
+    #         print(squares[(x, y)], end="")
+    #     print()
     print(largest_area)
     # print(max_x - min_x)
     # print(max_y - min_y)
